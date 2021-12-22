4037486a5adf22c79caf7e64e927dcc463d90d8b
Catherine Tranfield <victoria.tranfield@gmail.com>
2021-12-22 01:35:23 +0000

    unit tests added to test_PlayingCard.py for shuffle_cards, deal_a_card, trentine_small functions

--- test/test_playingCard.py
+++ test/test_playingCard.py
@@ -5,3 +5,20 @@ class PlayingCardTest(unittest.TestCase):
 
     def test_generate_deck(self):
         self.assertEqual(52, len(generate_deck()))
+
+    def test_shuffle_cards(self):
+        self.assertNotEqual(shuffle_cards(generate_deck()),
+                            shuffle_cards(generate_deck()))
+
+    def test_deal_a_card(self):
+        self.assertEquals('D6', deal_a_card(['S4', 'C9', 'D6']))
+
+    def test_trentine_small(self):
+        # new_card = PlayingCard()
+
+        self.assertEquals(faces, ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"])
+
+        trentine_small()
+        self.assertEquals(faces, ["A", "2", "3", "4", "5", "6", "7", "J", "Q", "K"])
+
+        # 4 cards, 4 players - each player has 1
