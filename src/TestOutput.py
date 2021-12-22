d6da3edbf42235e4d4cceb2a70d65954b429fc8c
Catherine Tranfield <victoria.tranfield@gmail.com>
2021-12-22 00:57:50 +0000

    Test Output added

---
+++ src/TestOutput.py
@@ -0,0 +1,13 @@
+from src.Output import Output
+
+class TestOutput(Output):
+
+    list_of_test_output = []
+
+    def get_list_of_test_output(self, list_of_test_output):
+        # return self.list_of_test_output
+        self.list_of_test_output = list_of_test_output
+
+    def display(self, message):
+        self.list_of_test_output.append(message)
+        return self.list_of_test_output
