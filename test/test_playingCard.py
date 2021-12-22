f4c2be6d63ed017d1446137d0a3e2f18f8f40b7c
Catherine Tranfield <victoria.tranfield@gmail.com>
2021-12-22 01:38:35 +0000

    unit tests added to test_PlayingCard.py for deal_cards_one_card_each, deal_cards_two_cards_each, play_a_card, is_playing_a_card, is_not_playing_a_card test cases

--- test/test_playingCard.py
+++ test/test_playingCard.py
@@ -22,3 +22,54 @@ class PlayingCardTest(unittest.TestCase):
         self.assertEquals(faces, ["A", "2", "3", "4", "5", "6", "7", "J", "Q", "K"])
 
         # 4 cards, 4 players - each player has 1
+
+    def test_deal_cards_one_card_each(self):
+        deck = ["1", "2", "3", "4"]
+
+        # new_card = PlayingCard()
+
+        hands = deal_cards(deck, 1, 4)
+
+        # 4 arrays
+        # each array - 1 element
+        # first player gets last card
+
+        self.assertEquals(hands, [["4"], ["3"], ["2"], ["1"]])
+
+    def test_deal_cards_two_cards_each(self):
+        deck = ["1", "2", "3", "4"]
+
+        # new_card = PlayingCard()
+
+        hands = deal_cards(deck, 2, 2)
+
+        # 4 arrays
+        # each array - 1 element
+        # first player gets last card
+
+        self.assertEquals(hands, [["4", "3"], ["2", "1"]])
+
+    def test_play_a_card(self):
+        # new_card = PlayingCard()
+
+        hand = ["1", "2", "3"]
+
+        play_a_card(hand, "3")
+
+        self.assertEquals(hand, ["1", "2"])
+
+    def test_is_playing_a_card(self):
+        # new_card = PlayingCard()
+
+        hand = ["1", "2", "3"]
+
+        self.assert_(is_playing_a_card(hand, "2"))
+        self.assertEquals(hand, ["1", "3"])
+
+    def test_is_not_playing_a_card(self):
+        # new_card = PlayingCard()
+
+        hand = ["1", "2", "3"]
+
+        self.assertFalse(is_playing_a_card(hand, "4"))
+        self.assertEquals(hand, ["1", "2", "3"])
